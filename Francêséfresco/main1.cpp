a1356bd9d5ab878bb37ba26b3d2237e3ef4b9834
hyarlei Silva <hyarleysf@gmail.com>
2023-03-26 20:50:34 -0300

    Updated de Exercicios envolvendo conteúdos vistos em sala

---
+++ Francêséfresco/main1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string frase;
+    // vai pegar tudo que for digitado e aramazenar na variável
+    getline(cin,frase);
+    // percorrer a frase
+    for(int i = 0; i < (int) frase.length(); i++){
+        // condicionais para juntar as palavras, se for igual a vogal ou se o índice seguinte é um espaço ou após o espaço é uma letra igual a atual
+        if((frase[i] == 'a' || frase[i] == 'e' || frase[i] == 'i' || frase[i] == 'o' || frase[i] == 'u' ) && frase[i+1] == ' ' && frase[i] == frase[i+2]){
+            // imprimi
+            cout << frase[i];
+            char vogal = frase[i];
+            // enquanto "vogal" == a letra ou for um espaço, continua o incremento;
+            while ((frase[i] == vogal) || (frase[i] == ' ')){
+                i++;
+            }
+            // saiu do enquanto, retorna uma letra e continua o laço
+            i--;
+        }else{
+            cout << frase[i];
+        }
+    }
+    
+}
