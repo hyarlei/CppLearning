dc8da12a9bba52dc9c924f30da79f78f30c250f7
hyarlei Silva <hyarleysf@gmail.com>
2023-04-07 12:10:14 -0300

    Triâgulo de somas

---
+++ TriângulodeSomas/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+""
+using namespace std;
+
+void printTriangle(vector<int> v) {
+    int n = v.size();
+    if (n == 1) { // caso base
+        cout << "[" << v[0] << "]" << endl;
+        return;
+    }
+    vector<int> v_new(n-1);
+    for (int i = 0; i < n-1; i++) {
+        v_new[i] = v[i] + v[i+1];
+    }
+    printTriangle(v_new);
+    cout << "[";
+    for (int i = 0; i < n-1; i++) {
+        cout << v[i] << ", ";
+    }
+    cout << v[n-1] << "]" << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    printTriangle(v);
+    return 0;
+}
