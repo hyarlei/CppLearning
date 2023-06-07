e118d6d8e3507c076f9896a28e5c980d5e801de2
hyarlei Silva <hyarleysf@gmail.com>
2023-06-07 18:46:27 -0300

    Finish - Trabalho de ED

--- Trabalho_ED/Trabalho_ED/main.cpp
+++ Trabalho_ED/Trabalho_ED/main.cpp
@@ -9,12 +9,12 @@ using namespace std;
 
 /* comandos:
 
-obs: para preencher matriz através do arquivo é necessário que a matriz já tenha sido criada
+Para preencher a Matriz através do arquivo é necessário que a matriz já tenha sido criada
 Ex:
 g++ main.cpp matrizfuncoes.cpp -o main
 ./main
-Para criar - Ex: 1 4 4
-Ler arquivo - Ex: 2 0 A.txt */
+Para criar -> Ex: 1 4 4
+Ler arquivo -> Ex: 2 0 matriz1.txt */
 
 void limparTela()
 {
@@ -25,7 +25,7 @@ void mostrarMenu()
 {
     cout << "======================================================== Menu ===================================================================" << endl;
     cout << "|   1. Criar matriz - (Informar tamanho da matriz - Ex: 1 4 4)                                                                  |" << endl;
-    cout << "|   2. Ler matriz de arquivo - (Número da matriz a ser preenchida e o nome do arquivo e extensão - Ex: 2 0 matriz1.txt)           |" << endl;
+    cout << "|   2. Ler matriz de arquivo - (Número da matriz a ser preenchida e o nome do arquivo e extensão - Ex: 2 0 matriz1.txt)         |" << endl;
     cout << "|   3. Inserir valor na matriz - (Número da matriz) n (Número da linha) n (Número da coluna) n (Valor) Ex: 0 1 1 99             |" << endl;
     cout << "|   4. Verificar valor na matriz - (Número da matriz) n (linha) n (coluna) Ex: 4 0 1 1                                          |" << endl;
     cout << "|   5. Somar matrizes - (Número da primeira matriz) n (Número da segunda matriz) Ex: 5 0 1                                      |" << endl;
@@ -58,17 +58,7 @@ int main()
             tokens.push_back(buffer);
         }
 
-        if (tokens[0] == "libera" || tokens[0] == "exit")
-        {
-            for (int i = 0; i < matrizes.size(); ++i)
-            {
-                delete matrizes[i];
-                matrizes[i] = nullptr;
-            }
-            matrizes.clear();
-            break;
-        }
-        // cria matriz.
+        // Cria matriz.
         else if (tokens[0] == "1")
         {
             int l = stoi(tokens[1]);
@@ -79,28 +69,8 @@ int main()
             limparTela();
             cout << "Matriz " << matrizes.size() - 1 << " criada com sucesso!" << endl;
         }
-        // imprime matriz
-        else if (tokens[0] == "7")
-        {
-            limparTela();
-            int k = stoi(tokens[1]);
-            matrizes[k]->print();
 
-            cout << "Matriz " << k << " impressa com sucesso!" << endl;
-        }
-        // insere valor na matriz
-        else if (tokens[0] == "3")
-        {
-            int k = stoi(tokens[1]);
-            int i = stoi(tokens[2]);
-            int j = stoi(tokens[3]);
-            double v = stoi(tokens[4]);
-            matrizes[k]->insert(i, j, v);
-
-            limparTela();
-            cout << "Valor " << v << " inserido na matriz " << k << " com sucesso!" << endl;
-        }
-        // ler arquivo
+        // Ler arquivo
         else if (tokens[0] == "2")
         {
             int k = stoi(tokens[1]);
@@ -118,7 +88,34 @@ int main()
             limparTela();
             cout << "Matriz " << k << " lida com sucesso!" << endl;
         }
-        // soma matrizes
+
+        // Insere valor na matriz
+        else if (tokens[0] == "3")
+        {
+            int k = stoi(tokens[1]);
+            int i = stoi(tokens[2]);
+            int j = stoi(tokens[3]);
+            double v = stoi(tokens[4]);
+            matrizes[k]->insert(i, j, v);
+
+            limparTela();
+            cout << "Valor " << v << " inserido na matriz " << k << " com sucesso!" << endl;
+        }
+
+        // Verifica valor na matriz
+        else if (tokens[0] == "4")
+        {
+            int k = stoi(tokens[1]);
+            int i = stoi(tokens[2]);
+            int j = stoi(tokens[3]);
+            double v = matrizes[k]->getValue(i, j);
+            cout << "valor: " << v << endl;
+
+            limparTela();
+            cout << "Valor " << v << " verificado na matriz " << k << " com sucesso!" << endl;
+        }
+
+        // Soma matrizes
         else if (tokens[0] == "5")
         {
             limparTela();
@@ -136,7 +133,8 @@ int main()
 
             cout << "Matriz " << p << " somada com a matriz " << q << " com sucesso!" << endl;
         }
-        // multiplica matrizes
+
+        // Multiplica matrizes
         else if (tokens[0] == "6")
         {
             limparTela();
@@ -153,19 +151,18 @@ int main()
 
             cout << "Matriz " << p << " multiplicada com a matriz " << q << " com sucesso!" << endl;
         }
-        // verifica valor na matriz
-        else if (tokens[0] == "4")
-        {
-            int k = stoi(tokens[1]);
-            int i = stoi(tokens[2]);
-            int j = stoi(tokens[3]);
-            double v = matrizes[k]->getValue(i, j);
-            cout << "valor: " << v << endl;
 
+        // Imprime matriz
+        else if (tokens[0] == "7")
+        {
             limparTela();
-            cout << "Valor " << v << " verificado na matriz " << k << " com sucesso!" << endl;
+            int k = stoi(tokens[1]);
+            matrizes[k]->print();
+
+            cout << "Matriz " << k << " impressa com sucesso!" << endl;
         }
 
+        // Sair
         else if (tokens[0] == "8")
         {
             limparTela();
